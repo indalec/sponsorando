7ebce9deaaa0d3abbdad1858a3de272bad4dacde
Desiree Valdes <desireevaldes@gmail.com>
2025-01-28 13:15:25 +0100

    Add sign up

--- src/main/java/com/sponsorando/app/controller/AppController.java
+++ src/main/java/com/sponsorando/app/controller/AppController.java
@@ -1,21 +1,60 @@
 package com.sponsorando.app.controller;
 
+import com.sponsorando.app.models.UserForm;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.security.Principal;
 
 @Controller
 public class AppController {
 
-
-   @GetMapping("/")
+    @GetMapping("/")
     public String index() {
-       return "index";
-   }
+        return "index";
+    }
 
-   @GetMapping("/login")
+    @GetMapping("/login")
     public String login() {
         return "login";
-   }
+    }
 
+    @GetMapping("/signup")
+    public String signup() {
+        return "signup";
+    }
 
+    @PostMapping("/signup")
+    public String postSignUp(
+            @ModelAttribute @Valid UserForm userForm,
+            RedirectAttributes redirectAttributes,
+            Model model
+    ){
+        System.out.println("SIGNUP USER: " + userForm);
+        redirectAttributes.addFlashAttribute("isSignedUp", true);
+        return "redirect:/";
+    }
+
+    @GetMapping("/logout")
+    public String logout(
+            HttpServletResponse response,
+            Model model
+    ) {
+        Cookie cookie = new Cookie("JSESSIONID", null);
+        cookie.setHttpOnly(true);
+        cookie.setPath("/");
+        cookie.setMaxAge(0);
+
+        response.addCookie(cookie);
+
+        model.addAttribute("username", null);
+        return "redirect:/";
+    }
 }
--- src/main/java/com/sponsorando/app/models/UserForm.java
+++ src/main/java/com/sponsorando/app/models/UserForm.java
@@ -1,5 +1,75 @@
 package com.sponsorando.app.models;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import org.springframework.web.multipart.MultipartFile;
 
 public class UserForm {
+    private Long id;
+
+    @NotBlank(message = "Name is required")
+    @Size(min = 2, max = 50, message = "Name must be between 2 and 50 characters")
+    private String name;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 8, message = "Password must be at least 8 characters long")
+    private String password;
+
+    private MultipartFile imageUrl;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public MultipartFile getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(MultipartFile imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    @Override
+    public String toString() {
+        return "UserForm{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", imageUrl=" + imageUrl +
+                '}';
+    }
 }
