e24fafbdc77cddda095d4adb05d04100c0304b7b
RobinHagolani <robin.hagolani@dci-student.org>
2025-01-30 13:40:56 +0100

    added add_campaign

--- src/main/java/com/sponsorando/app/controller/CampaignController.java
+++ src/main/java/com/sponsorando/app/controller/CampaignController.java
@@ -1,9 +1,14 @@
 package com.sponsorando.app.controller;
 
+import com.sponsorando.app.models.CampaignCategory;
+import com.sponsorando.app.repositories.CampaignCategoryRepository;
 import com.sponsorando.app.services.UserAccountService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class CampaignController {
@@ -11,8 +16,16 @@ public class CampaignController {
     @Autowired
     private UserAccountService userAccountService;
 
+    @Autowired
+    private CampaignCategoryRepository campaignCategoryRepository;
+
+
+
     @GetMapping("/add_campaign")
-    public String addCampaign(){
+    public String addCampaign(Model model) {
+        List<CampaignCategory> categories = campaignCategoryRepository.findAll();
+        model.addAttribute("categories", categories);
+
         return "add_campaign";
     }
 
