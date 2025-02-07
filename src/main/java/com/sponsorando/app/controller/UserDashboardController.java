3212add936afce53dc053a7c248b3f6b87af6cfb
RobinHagolani <robin.hagolani@dci-student.org>
2025-02-07 11:43:47 +0100

    styling of links to user_dashboard

---
+++ src/main/java/com/sponsorando/app/controller/UserDashboardController.java
@@ -0,0 +1,34 @@
+package com.sponsorando.app.controller;
+
+import com.sponsorando.app.models.UserAccount;
+import com.sponsorando.app.services.UserAccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.security.Principal;
+
+@Controller
+public class UserDashboardController {
+
+    @Autowired
+    private UserAccountService userAccountService;
+
+
+    @GetMapping("/user_dashboard")
+    public String userDashboard(Model model, Principal principal) {
+
+        String email = principal.getName();
+        UserAccount userAccount = userAccountService.getUser(email);
+
+        model.addAttribute("userAccount", userAccount);
+
+        return "user_dashboard";
+    }
+
+
+
+
+}
