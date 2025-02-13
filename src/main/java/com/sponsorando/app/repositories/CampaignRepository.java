6534cf93dd5ae015ea7f20460cac133357a5d802
Sunanda-Kodavali <sunanda.kodavali@gmail.com>
2025-02-13 10:55:31 +0100

    live search added

--- src/main/java/com/sponsorando/app/repositories/CampaignRepository.java
+++ src/main/java/com/sponsorando/app/repositories/CampaignRepository.java
@@ -17,7 +17,17 @@ public interface CampaignRepository extends JpaRepository<Campaign, Long > {
     Page<Campaign> findByUserAccountEmailAndStatusNot(String email, CampaignStatus status, Pageable pageable);
     long countByUserAccountEmailAndStatusNot(String email, CampaignStatus status);
     Optional<Campaign> findBySlug(String slug);
-    Page<Campaign> findByStatusAndTitle(CampaignStatus status, String title, Pageable pageable);
+    @Query("SELECT c FROM Campaign c " +
+            "JOIN c.categories cat " +
+            "WHERE c.status = :status " +
+            "AND (LOWER(c.title) LIKE LOWER(CONCAT('%', :title, '%')) " +
+            "OR LOWER(cat.name) LIKE LOWER(CONCAT('%', :category, '%')))")
+    Page<Campaign> findByStatusAndTitleContainingIgnoreCaseOrStatusAndCategoryContainingIgnoreCase(
+            @Param("status") CampaignStatus status,
+            @Param("title") String title,
+            @Param("category") String category,
+            Pageable pageable);
+
 
     Page<Campaign> findByStatus(CampaignStatus status, Pageable pageable);
 
