f22383a9b9dc454b85ff75de743329c1740a8e31
RobinHagolani <robin.hagolani@dci-student.org>
2025-02-24 13:47:29 +0100

    implemented validation of sign up fields

--- src/main/resources/static/js/signup.js
+++ src/main/resources/static/js/signup.js
@@ -1,10 +1,22 @@
 document.addEventListener("DOMContentLoaded", function () {
-
     const signupButtons = document.querySelectorAll("#signup, #signup-expanded");
 
     signupButtons.forEach(button => {
         if (button) {
             button.style.display = "none";
         }
+    });
+
+    // Form Validation Logic on Submit
+
+    const form = document.querySelector('.needs-validation'); // Select the form
+    form.addEventListener('submit', function (event) {
+
+        if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+        }
+
+        form.classList.add('was-validated'); // Add class to show feedback
     });
 });
